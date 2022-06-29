ce93351c41d155058827b69c387b181aec1d56d2
Daniel Rodrigues <inf.daniel@gmail.com>
2022-06-28 21:21:35 -0300

    feat: Creating a Smarter Cart

--- app/controllers/store_controller.rb
+++ app/controllers/store_controller.rb
@@ -4,5 +4,12 @@
 class StoreController < ApplicationController
   def index
     @products = Product.order(:title)
+    if session[:counter].nil?
+      session[:counter] = 0
+    else
+      counter = session[:counter] + 1
+      session[:counter] = counter
+    end
+    puts "Count: #{session[:counter]}"
   end
 end
